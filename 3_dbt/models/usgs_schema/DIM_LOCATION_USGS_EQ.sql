a4ca1cfc12c421b557277de19eed43f648010f3e
Renan Duarte <114159104+erreduarte@users.noreply.github.com>
2024-10-18 14:02:12 +0100

    Create DIM_LOCATION_USGS_EQ.sql

---
+++ 3_dbt/models/usgs_schema/DIM_LOCATION_USGS_EQ.sql
@@ -0,0 +1,22 @@
+WITH SOURCE_DATA AS (
+    SELECT 
+        ID, 
+        REGISTERED_AT, 
+        PLACE, 
+        GEO_TYPE, 
+        LATITUDE, 
+        LONGITUDE, 
+        ALTITUDE
+    FROM
+        {{ source('stg_usgs', 'STG_EARTHQUAKE_HISTORY') }}
+)
+
+SELECT * 
+FROM SOURCE_DATA A1
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM {{ this }} A2
+    WHERE 
+        A1.ID = A2.ID
+        AND A1.REGISTERED_AT = A2.REGISTERED_AT
+)
