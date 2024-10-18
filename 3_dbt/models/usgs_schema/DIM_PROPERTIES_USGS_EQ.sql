6b42d4c4bb6af885bf91e44c61e916269c6bb1f7
Renan Duarte <114159104+erreduarte@users.noreply.github.com>
2024-10-18 14:01:29 +0100

    Create DIM_PROPERTIES_USGS_EQ.sql

---
+++ 3_dbt/models/usgs_schema/DIM_PROPERTIES_USGS_EQ.sql
@@ -0,0 +1,25 @@
+WITH SOURCE_DATA AS (
+    SELECT 
+        ID,
+        REGISTERED_AT,
+        TYPE,
+        TITLE,
+        URL,
+        DETAIL,
+        NET,
+        SOURCES,
+        TYPES,
+        MAGTYPE
+    FROM
+        {{ source('stg_usgs', 'STG_EARTHQUAKE_HISTORY') }}
+)
+
+SELECT * 
+FROM SOURCE_DATA A1
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM {{ this }} A2
+    WHERE 
+        A1.ID = A2.ID
+        AND A1.REGISTERED_AT = A2.REGISTERED_AT
+)
